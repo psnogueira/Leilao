d1248c73ddaf6749e0abb7f734d4d782c4e711d7
PEDRO <lorddangel08@gmail.com>
2023-02-26 19:57:27 -0300

    Teste de Unidade "Avaliador"

--- Leilao/Avaliador.cpp
+++ Leilao/Avaliador.cpp
@@ -3,8 +3,12 @@
 void Avaliador::avalia(Leilao leilao)
 {
 	std::vector<Lance> lances = leilao.recuperaLances();
-	Lance ultimoLance = lances.back();
-	maiorValor = ultimoLance.recuperaValor();
+	
+	for (Lance lance : lances)
+	{
+		if (lance.recuperaValor() > maiorValor)
+			maiorValor = lance.recuperaValor();
+	}
 }
 
 float Avaliador::recuperaMaiorValor() const
--- Leilao/teste-avaliador.cpp
+++ Leilao/teste-avaliador.cpp
@@ -2,17 +2,31 @@
 #include <iostream>
 
 int main() {
+	// Arrange - Given (Preparando ambiente)
 	Lance primeirolance(Usuario("Pedro"), 1000);
 	Lance segundolance(Usuario("Ana Maria"), 2000);
+	Lance terceirolance(Usuario("Sophia"), 1500);
 	Leilao leilao("Broinha 1554 Premium");
-	leilao.recebeLance(primeirolance);
 	leilao.recebeLance(segundolance);
-
+	leilao.recebeLance(terceirolance);
+	leilao.recebeLance(primeirolance);
+	
 	Avaliador leiloeiro;
+
+	// Act - When (Excutando o código a ser testado)
 	leiloeiro.avalia(leilao);
 
-	std::cout << leiloeiro.recuperaMaiorValor() << 
-		std::endl;
+	// Assert - Then (Verificando se tudo deu certo)
+	float valorEsperado = 2000;
+
+	if (valorEsperado == leiloeiro.recuperaMaiorValor())
+	{
+		std::cout << "TESTE OK" << std::endl;
+	}
+	else
+	{
+		std::cout << "TESTE FALHOU" << std::endl;
+	}
 
 	return 0;
 }
