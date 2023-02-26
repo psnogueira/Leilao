96dbccc4b61cdcba0e238fc4e92befdd9305a72f
PEDRO <lorddangel08@gmail.com>
2023-02-26 20:43:07 -0300

    Teste de Unidade com Catch2

--- Leilao/teste-avaliador.cpp
+++ Leilao/teste-avaliador.cpp
@@ -1,15 +1,16 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 #include "Avaliador.hpp"
-#include <iostream>
 
-int main() {
+TEST_CASE("Deve recuperar maior lance de leilao em ordem crescente") {
 	// Arrange - Given (Preparando ambiente)
 	Lance primeirolance(Usuario("Pedro"), 1000);
-	Lance segundolance(Usuario("Ana Maria"), 2000);
-	Lance terceirolance(Usuario("Sophia"), 1500);
+	Lance segundolance(Usuario("Ana Maria"), 1500);
+	Lance terceirolance(Usuario("Sophia"), 2000);
 	Leilao leilao("Broinha 1554 Premium");
+	leilao.recebeLance(primeirolance);
 	leilao.recebeLance(segundolance);
 	leilao.recebeLance(terceirolance);
-	leilao.recebeLance(primeirolance);
 	
 	Avaliador leiloeiro;
 
@@ -17,16 +18,26 @@ int main() {
 	leiloeiro.avalia(leilao);
 
 	// Assert - Then (Verificando se tudo deu certo)
-	float valorEsperado = 2000;
-
-	if (valorEsperado == leiloeiro.recuperaMaiorValor())
-	{
-		std::cout << "TESTE OK" << std::endl;
-	}
-	else
-	{
-		std::cout << "TESTE FALHOU" << std::endl;
-	}
-
-	return 0;
+	REQUIRE(2000 == leiloeiro.recuperaMaiorValor());
 }
+
+TEST_CASE("Deve recuperar maior lance de leilao em ordem decrescente") {
+	// Arrange - Given (Preparando ambiente)
+	Lance primeirolance(Usuario("Pedro"), 1000);
+	Lance segundolance(Usuario("Ana Maria"), 1500);
+	Lance terceirolance(Usuario("Sophia"), 2000);
+	Leilao leilao("Broinha 1554 Premium");
+	leilao.recebeLance(terceirolance);
+	leilao.recebeLance(segundolance);
+	leilao.recebeLance(primeirolance);
+
+	Avaliador leiloeiro;
+
+	// Act - When (Excutando o código a ser testado)
+	leiloeiro.avalia(leilao);
+
+	// Assert - Then (Verificando se tudo deu certo)
+	REQUIRE(2000 == leiloeiro.recuperaMaiorValor());
+}
+
+
